72cdfc67cf4ab64e1bdb7e819cd0842c2e8a5836
Sophist333 <57650277+Sophist333@users.noreply.github.com>
2020-06-30 22:37:36 +0900

    Update main.js

--- 詳解JavaScript_Canvas/js/main.js
+++ 詳解JavaScript_Canvas/js/main.js
@@ -46,7 +46,7 @@
     //   x0, y0, r0,
     //   x1, y1, r1
     // );
-    const g = ctx.createRadialGradient(
+    g = ctx.createRadialGradient(
       canvas.width / 2, canvas.height / 2, 50,
       canvas.width / 2 + 100, canvas.height / 2 + 200, 500
     );
