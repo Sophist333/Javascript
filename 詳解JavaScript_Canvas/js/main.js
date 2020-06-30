66d0eb60f504e65364bb41c759315db862e4e3e8
Sophist333 <57650277+Sophist333@users.noreply.github.com>
2020-06-30 22:59:20 +0900

    Update main.js

--- 詳解JavaScript_Canvas/js/main.js
+++ 詳解JavaScript_Canvas/js/main.js
@@ -46,7 +46,7 @@
     //   x0, y0, r0,
     //   x1, y1, r1
     // );
-    g = ctx.createRadialGradient(
+    const g = ctx.createRadialGradient(
       canvas.width / 2, canvas.height / 2, 50,
       canvas.width / 2 + 100, canvas.height / 2 + 200, 500
     );
@@ -57,6 +57,45 @@
     
     ctx.fillStyle = g;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    #影のつけ方
+    ctx.shadowOffsetX = 4;//ずらす幅
+    ctx.shadowOffsetY = 4;
+    ctx.shadowBlur = 4;//どれくらいぼかすか
+    ctx.shadowColor = 'rgba(0, 0 ,0, 0.3)';//透明度
+
+    ctx.fillRect(50, 50, 50, 50);
+    
+    #線の描画
+    ctx.beginPath();//新しいパスを開始
+    ctx.moveTo(50, 50);//50,50の位置から開始
+    ctx.lineTo(100, 50);
+    ctx.lineTo(100, 100);//2本の線
+    ctx.closePath();//終了
+    
+    // ctx.stroke();//線を表示
+    ctx.fill();//塗りつぶし
+    
+    #線のスタイル設定
+    ctx.beginPath();
+    ctx.moveTo(100, 50,);
+    ctx.lineTo(200, 50);
+    ctx.setLineDash([5, 10]);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(100, 100,);
+    ctx.lineTo(200, 100);
+    ctx.setLineDash([]);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(100, 150,);
+    ctx.lineTo(200, 150);
+    ctx.lineWidth = 16;
+    ctx.lineCap = 'round';
+    ctx.stroke();
+    
   }
 
   draw();
